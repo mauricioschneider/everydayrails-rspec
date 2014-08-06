bc9b6dd0f274dbcb8c641bfd1de48a29b37931e8
Mauricio Schneider <me@mauricioschneider.com>
2014-08-05 22:54:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EverydayrailsRspec::Application.config.secret_key_base = '0e8a3358ed51f79e063e145172320eb794f04354d1de34add9b2da3c026280812be6fb10344a3f4d4790f37e1df74a4475e32f37fca44fc07e4236ae5380fc0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EverydayrailsRspec::Application.config.session_store :cookie_store, key: '_everydayrails-rspec_session'
